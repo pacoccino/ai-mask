725517c75392da47883f37ff82115d36844e345c
pacoccino <pac@opac.me>
2024-02-23 14:46:23 +0100

    replace next by vite

--- examples/webai-app/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Main from "@/components/main";
-
-export default function Home() {
-  return (
-    <Main />
-  );
-}
--- examples/webai-app/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    experimental: {
-        externalDir: true,
-    },
-};
-
-export default nextConfig;
